890c37332d545aea6e5183723f6efdf105b82230
Anton <pozernishku@gmail.com>
2018-07-30 23:34:42 +0300

    dfs is added

---
+++ dfs.py
@@ -0,0 +1,15 @@
+def dfs(graph, vertex, path=[]):
+	path += [vertex]
+
+	for neighbor in graph[vertex]:
+		if neighbor not in path:
+			path = dfs(graph, neighbor, path)
+
+	return path
+
+# a sample graph
+G = {1: [2, 3], 2: [4, 5],
+	 3: [5], 4: [6], 5: [6],
+	 6: [7], 7: []}
+
+print(dfs(G, 1))
