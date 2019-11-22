12990d770b735b5e4108409ec6fd732e6cc96fd5
Anton <pozernishku@gmail.com>
2019-11-22 15:39:12 +0200

    Linked list new to learn

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,153 @@
+'''
+Below code is based on this repo: https://github.com/joeyajames/Python/tree/master/LinkedLists
+See also:
+CircularLinkedList.py
+DoublyLinkedList1.py
+DoublyLinkedList2.py
+'''
+
+class Node(object):
+    
+    def __init__(self, d, n=None):
+        self.data = d
+        self.next_node = n
+        
+    def get_next(self):
+        return self.next_node
+    
+    def set_next(self, n):
+        self.next_node = n
+        
+    def get_data(self):
+        return self.data
+    
+    def set_data(self, d):
+        self.data = d
+        
+    def to_string(self):
+        return "Node value: " + str(self.data)
+    
+    def has_next(self):
+        if self.get_next() is None:
+            return False
+        return True
+    
+    def compare_to(self, y):
+        if self.to_string() < y.to_string():
+            return -1
+        elif self.to_string() > y.to_string():
+            return 1
+        return 0
+
+class LinkedList(object):
+    
+    def __init__(self, r=None):
+        self.root = r
+        self.size = 0
+        
+    def get_size(self):
+        return self.size
+    
+    def add(self, d):
+        new_node = Node(d, self.root)
+        self.root = new_node
+        self.size += 1
+        
+    def add_node(self, n):
+        n.set_next(self.root)
+        self.root = n
+        self.size += 1
+        
+    def remove(self, d):
+        this_node = self.root
+        prev_node = None
+        
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node: # removing node that is not the root
+                    prev_node.set_next(this_node.get_next())
+                else: # removing root node
+                    self.root = this_node.get_next()
+                self.size -= 1
+                return True # data removed
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False # data not found
+    
+    '''Alternative find'''
+    # def find(self, d):
+    #     this_node = self.root
+    #     while this_node:
+    #         if this_node.get_data() == d:
+    #             return d
+    #         else:
+    #             this_node = this_node.get_next()
+    #     return None
+
+    '''Alternative find'''
+    def find (self, d):
+        this_node = self.root
+        while this_node is not None:
+            if this_node.get_data() == d:
+                return d
+            elif this_node.get_next() == None:
+                return False
+            else:
+                this_node = this_node.get_next()
+    
+    def print_list(self):
+        print('Print List...')
+        if self.root is None:
+            return
+        current = self.root
+        print(current.to_string())
+        while current.has_next():
+            current = current.get_next()
+            print(current.to_string())
+            
+    def sort(self):
+        if self.size > 1:
+            newlist = []
+            current = self.root
+            newlist.append(self.root)
+            while current.has_next():
+                current = current.get_next()
+                newlist.append(current)
+            newlist = sorted(newlist, key = lambda node: node.get_data(), reverse = True)
+            newll = LinkedList()
+            for node in newlist:
+                newll.add_node(node)
+            return newll
+        return self
+
+
+# Experementing...
+
+myList = LinkedList()
+myList.add(5)
+myList.add(9)
+myList.add(3)
+myList.add(8)
+myList.add_node(Node(11))
+myList.add(9)
+print("size="+str(myList.get_size()))
+myList.print_list()
+myList = myList.sort()
+myList.print_list()
+myList.remove(8)
+print("size="+str(myList.get_size()))
+print("Remove 15", myList.remove(15))
+print("size="+str(myList.get_size()))
+print("Find 25", myList.find(25))
+myList.print_list()
+
+
+
+
+'''
+Below code is old one
+'''
 class node:
     def __init__(self, data=None, next=None):
         self.data = data
@@ -51,11 +201,11 @@ class linked_list:
             node = node.next
 
 
+# Experementing with old code...
+# s = linked_list()
+# s.add_at_front(5)
+# s.add_at_end(8)
+# s.add_at_front(9)
+# s.delete_node(5)
 
-s = linked_list()
-s.add_at_front(5)
-s.add_at_end(8)
-s.add_at_front(9)
-s.delete_node(5)
-
-s.print_list()
+# s.print_list()
